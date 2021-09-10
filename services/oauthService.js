d0f38d5dfdff7891f0449a5770a4f2e645554df6
JuliaZamaitat <j.zamaitat@gmail.com>
2021-09-10 15:23:53 +0200

    test

--- services/oauthService.js
+++ services/oauthService.js
@@ -9,44 +9,38 @@ const refreshToken = process.env.OAUTH_REFRESH_TOKEN;
 
 const oauth2Client = new OAuth2(clientID, clientSecret, redirectURL);
 
-if (process.env.NODE_ENV == "production") {
-  oauth2Client.setCredentials({
-    refresh_token: refreshToken,
-  });
-  // eslint-disable-next-line no-unused-vars
-
-  const getToken = async () => {
-    const { token } = await oauth2Client.getAccessToken();
-
-    if (!token) {
-      return undefined;
-    }
-  };
-
-  let gmailAccessToken = getToken();
-
-  console.log("accesstoken", gmailAccessToken);
-  console.log("refreshToken", refreshToken);
-  let gmailTransporter;
-  if (gmailAccessToken) {
-    console.log("accesstoken2", gmailAccessToken);
-    gmailTransporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        type: "OAuth2",
-        user: "studyplanhtwberlin@gmail.com",
-        clientId: clientID,
-        clientSecret: clientSecret,
-        refreshToken: refreshToken,
-        accessToken: gmailAccessToken,
-        tls: {
-          rejectUnauthorized: false,
-        },
-      },
+const mail = async () => {
+  if (process.env.NODE_ENV == "production") {
+    oauth2Client.setCredentials({
+      refresh_token: refreshToken,
     });
-  }
+    // eslint-disable-next-line no-unused-vars
 
-  module.exports = {
-    gmailTransporter: gmailTransporter,
-  };
-}
+    let gmailAccessToken = await oauth2Client.getAccessToken();
+
+    console.log("accesstoken", gmailAccessToken);
+    console.log("refreshToken", refreshToken);
+    if (gmailAccessToken) {
+      console.log("accesstoken2", gmailAccessToken);
+      return nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          type: "OAuth2",
+          user: "studyplanhtwberlin@gmail.com",
+          clientId: clientID,
+          clientSecret: clientSecret,
+          refreshToken: refreshToken,
+          accessToken: gmailAccessToken,
+          tls: {
+            rejectUnauthorized: false,
+          },
+        },
+      });
+    }
+  }
+};
+let gmailTransporter = mail();
+
+module.exports = {
+  gmailTransporter: gmailTransporter,
+};
